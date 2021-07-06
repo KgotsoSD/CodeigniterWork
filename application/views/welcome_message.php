b433acc624ead1fdc038df7bbdaf084f1d2f7af6
Kgotso Maduna <kgmaduna01@gmail.com>
2021-07-06 14:44:01 +0200

    updated webpage

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -1,29 +1,27 @@
 
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
 	<meta charset="utf-8">
-	<title>Welcome to Kgotso Maduna Graphics</title>
+	<title>Welcome to Kgotso Maduna's portfolio page</title>
 
 	
 	<link rel="stylesheet" href ="<?php echo base_url();?>Assets/Styles/styles.css">
 	
 </head>
 
-<div class= "container">
-<body background=""class = "backgrnd" alt="">
+
+<body background="https://mir-s3-cdn-cf.behance.net/project_modules/1400_opt_1/3cfd38121156537.60c14c6703f22.jpg" >
 
 
 
-
-
-	<p style="color: pink;"><strong> Kgotso Maduna Graphics</strong></p>
+	<p style="color: white;"><strong style="margin-left:43%;"> Kgotso Maduna</strong></p>
 	<div class = "img-text-wrapper">
 					<div class= "logo-wrapper">
 						<img src= "Assets/Images/logo.jpg" 
-						class = "myLogo" alt="">
-	                 
+						class = "myLogo" alt=" style= width: 20%;
+				   
+	                 "style=" width:20%; margin-left:40%;">
                      </div>
                </div>
 
@@ -33,7 +31,7 @@
 	    <div class="topnav-right">
            <a href="#"class="li">Home</a>
             <a href="#About">About</a>
-			<a href="#Profile">Profile</a>
+			<a href="https://www.linkedin.com/in/kgotso-maduna-24a4641aa/">Profile</a>
        </div> 
 
 
@@ -49,8 +47,13 @@
 					<div class ="portfolio-items-wrapper">
 						<div class ="portfolio-item-wrapper">
 							<div class ="portfolio-img-background" style="background-image:url(Assets/Images/img2.jpg"></div>
-						<h5> We provide you with amazing graphic desig from your logos to your banners. </h5>
-						<h5> Trust us to bring you quality results</h5>
+						<h1>Hey, I am Kgotso :). A Software Development student at Tshwane University of Technology. </h1>
+						<h3>Welcome to my page!</h3>
+
+
+						<p><button>Contact</button></p>
+
+
                 </div>
     
 
@@ -65,22 +68,21 @@
 
 
  <div class="about" id="bout" style="height:600px;">
-	<p>hello</p>
+	
 </div>	
 <div class="about" id="bout" style="height:100px;">
- <img src="Assets/Images/img5.jpg" width=300 height=600>
+ 
 </div>	
+
 		
 <div class="about" id="About" style="height:900px">
-	<p>Kgotso Graphics was founded in 2021, with the aim of providing the world with top class pictures.</p>
+	<p>Kgotso Maduna is an aspiring Software Developer, who is willing to learn more about the Computer Science industry. She is currently learning towards a National Diploma in Information Technology, Software Development at Tshwane University of Technology. In addition to what was already mentioned, she can be described as an initiative-taking individual with passion for developing software. Her passion goes far more beyond working during office hours. She understands clearly that a career in Tech is more about continous learning and is extremely willing to sacrifice her time to encourage her growth in the Tech world.</p>
+
+	<p>She now is able to use languges like c++, and java and can work with web technologies such as HTML, CSS, as well as some Javascript. Currently she is learning how to use libraries such as Codeigniter, Laravel as well as react js. She is also learning how to use MVC framework, C#, and how to use APIs.</p>
 </div>	
 
 <div class="Profile" id="Profile" style="height:50%">
-	<p>Available pictures in 2021</p>
-	<img src="Assets/Images/img1.jpg" width=300 height=100>
-	<img src="Assets/Images/img2.jpg" width=300 height=100>
-	<img src="Assets/Images/img3.jpg" width=300 height=100>
-
+	
 </div>	
 		
 
